f5b7e3e33ed276777d5c07525e2037ac209ee2f6
Yehia-Fahmy <yehia.fahmy@uwaterloo.ca>
2020-12-31 18:52:23 -0500

    finished servo pot control

--- apps/tutorial-servo-pot-control/src/main.cpp
+++ apps/tutorial-servo-pot-control/src/main.cpp
@@ -1,7 +1,13 @@
 #include "mbed.h"
 
+AnalogIn potVoltageIn(PA_0);
+PwmOut servoPwmOut(PA_1);
+
 int main(int argc, char const *argv[]) {
+  servoPwmOut.period_ms(20);
   while (1) {
+    float potVoltage = potVoltageIn.read();
+    servoPwmOut.pulsewidth.ms(1 + potVoltage / 3.3)
   }
   return 0;
 }
